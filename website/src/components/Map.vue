05c3ecd280cb2fff9f9a2a18c385b07fd41a9ac3
Li <liling@kth.se>
2019-03-02 17:44:46 +0100

    Add placeholder map component

---
+++ website/src/components/Map.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="map">
+      <h3>Placeholder for map with data: {{filename}}</h3>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Map',
+  props: {
+    filename: String
+  }
+}
+</script>
