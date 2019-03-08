68d37a67d69907e32dbd211f9c0f2e51f473adc1
Li <liling@kth.se>
2019-03-08 14:34:42 +0100

    #17 Add timeline component

---
+++ website/src/components/Timeline.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="timeline">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Timeline',
+  props: ['range'],
+    data() {
+        return {
+        }
+    },
+    methods: {
+    },
+    mounted() {
+    }
+}
+</script>
+
+<style>
+
+</style>
+
