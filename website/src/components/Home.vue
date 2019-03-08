54d5da79df6d71c4bcfd8334c34b73682ebc3fdb
Li <liling@kth.se>
2019-03-08 18:05:05 +0100

    Fix #17: working timeline (map and search bar updates on mouse release)

--- website/src/components/Home.vue
+++ website/src/components/Home.vue
@@ -18,9 +18,11 @@
                     v-on:selection-change="scale=$event"/>
             </div>
         </div>
-        <Map v-bind:ratingsJSON="ratingsJSON" v-bind:movieJSON="movieJSON" v-bind:movieid="movieid"
-            v-bind:mode="mode" v-bind:scale="scale"/>
-        <Timeline v-on:selection-change="year=$event"/>
+        <Map v-bind:movieJSON="movieJSON" v-bind:movieid="movieid"
+            v-bind:mode="mode" v-bind:scale="scale" v-bind:year="year"/>
+        <Timeline v-on:selection-change="year=$event"
+                        v-on:deselect-year="year='2000 - 2019'"
+        />
       </div>
       <div class="col-sm-4">
         <Searchbar v-bind:movieList="movieList"
@@ -37,8 +39,6 @@ import Map from '@/components/Map.vue'
 import Searchbar from '@/components/Searchbar.vue'
 import Timeline from '@/components/Timeline.vue'
 import movieJSON from '../../../database/merged_data_readable.json';
-import testJSON from '../../../database/test.json';
-import ratingsJSON from '../../../database/average_rating.json';
 import * as d3 from 'd3';
 
 export default {
@@ -54,18 +54,22 @@ export default {
           mode: 'revenue',
           scale: 'total',
           movieid: '',
-          year: 2000,
+          year: '2000 - 2019',
           movieJSON: movieJSON,
           movieList: Object.values(movieJSON),
-          ratingsJSON: ratingsJSON
       }
   },
   watch: {
       year: function() {
           var tmp = this.year;
-          this.movieList = Object.values(movieJSON).filter(function (el) {
-              return el.release_year == tmp;
-          })
+
+          if (tmp == '2000 - 2019') {
+              this.movieList = Object.values(movieJSON);
+          } else {
+              this.movieList = Object.values(movieJSON).filter(function (el) {
+                  return el.release_year == tmp;
+              })
+          }
       }
   },
   computed: {
--- website/src/components/Timeline.vue
+++ website/src/components/Timeline.vue
@@ -2,16 +2,19 @@
     <div class="timeline">
         <div class="row">
             <div class="col-sm-1">
-                <p style="margin:5px">Select Year:</p>
+                <p style="margin: 5px; font-weight:bold">Select Year:</p>
             </div>
-            <div class="col-sm-11">
+            <div class="col-sm-9">
                 <b-form-input class="custom-range" type="range" id="timeline" v-model="year"
-                            min="2000" max="2019" step="1"
+                            min="2000" max="2020" step="1"
                             v-on:change="signalChange"
                             />
             </div>
+            <div class="col-sm-1">
+                <b-button v-on:click="deselectYear" style="margin-right:2px">Deselect Year</b-button>
+            </div>
         </div>
-<p>Selected year: {{year}}</p>
+<p>Selected year: {{yearString}}</p>
     </div>
 </template>
 
@@ -23,16 +26,24 @@ export default {
         return {
             value: 0,
             year: 2000,
+            yearStringDefault: '2000 - 2019',
+            yearString: '',
             yearRange: Array.from({length: 19}, (x, i) => 2000 + i)
         }
     },
     methods: {
         signalChange: function(evt) {
-            console.log(evt)
+            this.yearString = this.year;
             this.$emit('selection-change',evt);
+        },
+        deselectYear: function(evt) {
+            this.$emit('deselect-year');
+            this.year = 2020;
+            this.yearString = this.yearStringDefault;
         }
     },
     mounted() {
+        this.yearString = this.yearStringDefault;
     }
 }
 </script>
